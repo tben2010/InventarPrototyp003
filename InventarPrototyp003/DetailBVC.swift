6f6a09c129cdc1264e2ef1f82bce848a2fdb8a93
Tim Bengsch <tbengsch@ish.de>
2016-02-18 11:43:32 +0100

    View Detail B eingefügt

---
+++ InventarPrototyp003/DetailBVC.swift
@@ -0,0 +1,35 @@
+//
+//  DetailBVC.swift
+//  InventarPrototyp003
+//
+//  Created by tben on 18.02.16|KW7.
+//  Copyright © 2016 tben. All rights reserved.
+//
+
+import UIKit
+
+class DetailBVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
