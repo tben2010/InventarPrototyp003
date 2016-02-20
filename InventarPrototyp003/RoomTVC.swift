ed964c8cd3617c90a754d29e71455d8757bb8346
Tim Bengsch <tbengsch@ish.de>
2016-02-20 13:09:11 +0100

    Aufgeräumt

--- InventarPrototyp003/RoomTVC.swift
+++ InventarPrototyp003/RoomTVC.swift
@@ -40,27 +40,5 @@ class RoomTVC: UITableViewController {
         cell.textLabel?.text = "Zeile ist gleich = \(indexPath.row)"
         return cell
     }
-    
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    // MARK: - Navigation
-    /*
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
